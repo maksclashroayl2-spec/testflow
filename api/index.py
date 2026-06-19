831a56ec4c98ca2ea91d1f03ded7d5059fa39146
Farkhad <testflow1@mail.ru>
2026-06-19 17:54:32 +0600

    Add Vercel deployment config

---
+++ api/index.py
@@ -0,0 +1,10 @@
+import os
+import sys
+
+sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'testflow_project.settings')
+
+from testflow_project.wsgi import application
+
+app = application
