07d09b03bd475bd88c105e494f8159c584e06014
Farkhad <testflow1@mail.ru>
2026-06-19 19:40:34 +0600

    Set default EMAIL_HOST_USER for SMTP

--- testflow_project/settings.py
+++ testflow_project/settings.py
@@ -228,8 +228,8 @@ EMAIL_TIMEOUT = 10
 EMAIL_USE_TLS = os.getenv("EMAIL_USE_TLS", "True") == "True"
 EMAIL_USE_SSL = os.getenv("EMAIL_USE_SSL", "False") == "True"
 
-EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
-EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
+EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER", "testflow.platform@gmail.com")
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD", "")
 
 DEFAULT_FROM_EMAIL = os.getenv(
     "DEFAULT_FROM_EMAIL",
